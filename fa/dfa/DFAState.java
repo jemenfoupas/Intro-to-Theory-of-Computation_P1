c04b38c169e0583ab877ab9ce9cd54bed4247ee3
Rich <boundjirichrich@gmail.com>
2022-09-14 20:19:04 -0600

    close #13

--- fa/dfa/DFAState.java
+++ fa/dfa/DFAState.java
@@ -1,11 +1,27 @@
 package fa.dfa;
 
+import java.util.HashMap;
+
+import javax.print.attribute.HashDocAttributeSet;
+
 import fa.State;
 
 public class DFAState extends State {
 
+    private HashMap<Character, String> transitions;
+
     public DFAState(String name) {
         this.name = name;
+        transitions = new HashMap<>();
+    }
+
+    public String getTransitions(char Character) {
+        String nextState = transitions.get(Character);
+        return nextState;
+    }
+
+    public void addTransitions(){
+
     }
 
     
